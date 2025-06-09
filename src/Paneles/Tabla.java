0ad01133887acb80bac21ac7dc2f164954ab7402
Pollo <erick@localhost.localdomain>
2025-06-09 10:20:22 -0600

    Pacientes

--- src/Paneles/Tabla.java
+++ src/Paneles/Tabla.java
@@ -1,4 +1,6 @@
-package vistas;
+package Paneles;
+
+import data.Pacientes;
 
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
@@ -8,11 +10,9 @@ public class Tabla extends JScrollPane {  // Ahora es un componente JScrollPane
     public Tabla() {
         // Columnas y datos
         String[] columnas = {"ID", "Nombre", "Edad"};
-        Object[][] datos = {
-                {1, "Ana", 20},
-                {2, "Luis", 22},
-                {3, "María", 19}
-        };
+
+        Object[][] datos =  new Pacientes().getDatos();
+
 
         // Crear modelo y tabla
         DefaultTableModel model = new DefaultTableModel(datos, columnas);
@@ -20,6 +20,6 @@ public class Tabla extends JScrollPane {  // Ahora es un componente JScrollPane
 
         // Configurar este JScrollPane para contener la tabla
         this.setViewportView(table);
-        this.setBounds(50, 50, 400, 200); // Posición y tamaño
+        this.setBounds(0, 0, 400, 800); // Posición y tamaño
     }
 }
