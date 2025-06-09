4be9b2af6f7a21e3fe433bdf38fa0ced11f3059b
Pollo <erick@localhost.localdomain>
2025-06-09 03:10:44 -0600

    Agrego Menu a Dashbord

--- src/ventanas/DashBoard.java
+++ src/ventanas/DashBoard.java
@@ -1,21 +1,24 @@
 package ventanas;
-import java.awt.Color;
 
+import Paneles.MenuLateralPanel;
+import Paneles.Tabla;
 import javax.swing.JFrame;
+import java.awt.*;
 
 public class DashBoard extends JFrame {
 
+    public DashBoard() {
+        setTitle("Doctor");
+        setSize(1900, 1000);
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        setLayout(new BorderLayout());
 
+        // Crear e integrar la tabla
+        this.add(new MenuLateralPanel().getPanel(), BorderLayout.WEST);
+        Tabla tabla = new Tabla();
+        add(tabla); // Agregar directamente al JFrame
 
-    public DashBoard(){
-        this.setTitle("DashBoard");
-        this.setSize(1920,1080);
         this.setVisible(true);
-        this.setBackground(Color.GREEN);
-
-
     }
-    public JFrame getJFrame(){
-        return this;
-    }
+
 }
